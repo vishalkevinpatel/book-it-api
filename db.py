470760332c9b881a4a280c1d4190cf2ed3ef781b
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2023-12-13 14:58:20 -0500

    seeded database

---
+++ db.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+
+def connect_to_db():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def initial_setup():
+    conn = connect_to_db()
+    conn.execute(
+        """
+        DROP TABLE IF EXISTS books;
+        """
+    )
+    conn.execute(
+        """
+        CREATE TABLE books (
+          id INTEGER PRIMARY KEY NOT NULL,
+          title TEXT,
+          author TEXT,
+          description TEXT,
+          image TEXT
+        );
+        """
+    )
+    conn.commit()
+    print("Table created successfully")
+
+    books_seed_data = [
+        ("Harry Potter and the Chamber of Secrets", "JK Rowling", "book 2", "https://upload.wikimedia.org/wikipedia/en/5/5c/Harry_Potter_and_the_Chamber_of_Secrets.jpg"),
+        ("Harry Potter and the Goblet of Fire", "JK Rowling", "book 4", "https://upload.wikimedia.org/wikipedia/en/b/b6/Harry_Potter_and_the_Goblet_of_Fire_cover.png"),
+        ("Harry Potter and the Order of the Phoenix", "JK Rowling", "book 5", "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Harry_Potter_and_the_Order_of_the_Phoenix.jpg/220px-Harry_Potter_and_the_Order_of_the_Phoenix.jpg"),
+    ]
+    conn.executemany(
+        """
+        INSERT INTO books (title, author, description, image)
+        VALUES (?,?,?,?)
+        """,
+        books_seed_data,
+    )
+    conn.commit()
+    print("Seed data created successfully with books")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    initial_setup()
